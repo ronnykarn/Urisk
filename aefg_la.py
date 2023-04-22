a82eb9f473f199bc794133ec201a2d00155cd1d6
arun-particlephy <arun.particlephy@tamu.edu>
2023-04-22 10:42:18 -0500

    single residence AEFG analysis

---
+++ aefg_la.py
@@ -0,0 +1,71 @@
+# evaluate the revenue lost by the utility in the presence of BTM DER
+
+import numpy as np
+import pandas as pd
+from import_data import import_data
+from res_unit import ResUnit
+from revenue_loss import RevenueLoss
+import random as r
+
+r.seed(227)
+
+# import data into a dictionary
+data = import_data()
+
+# input for net metering
+netMetering = input("Does the utility employ net metering?(Y/N)")
+
+# simulation for LA county
+testCounty = 'Los Angeles County'
+peakLoad = 4.5
+laResUnit = ResUnit(load_profile=data['countyLoadProfiles'][testCounty],
+                    ghi=data['countyGHI'][testCounty],
+                    pv_module_rating=data['PVModuleRating'],
+                    es_module_rating=data['ESModuleRating'],
+                    soc_min=data['socMin'],
+                    peak_load=peakLoad,
+                    retail_price=0.2231,
+                    net_metering_price=0,
+                    norm=False)
+laResUnit.ESPowerLimit = 2*peakLoad
+
+if netMetering == 'Y' or 'y':
+    laResUnit.netMeteringPrice = laResUnit.retailPrice / 2
+else:
+    laResUnit.netMeteringPrice = 0
+
+PVMax = 10.5
+PVStep = 1.5
+ESMax = 20
+ESStep = 2
+
+revenueLoss = pd.DataFrame(
+    columns=['PV(kW)', 'ES(kWh)', 'Revenue_loss']
+)
+
+for stepPV in range(int(PVMax/PVStep)):
+    PVSize = (stepPV+1)*PVStep
+    for stepES in range(int(ESMax/ESStep)):
+        ESSize = (stepES+1)*ESStep
+
+        # set PV and ES sizes and calculate net load
+        laResUnit.PVSize = PVSize
+        laResUnit.ESSize = ESSize
+        laResUnit.netLoadNoFailures()
+
+        # evaluate the lost revenue
+        lostRevenue = RevenueLoss(1.2, laResUnit.loadTimeSeries, laResUnit.netLoadNoFailures(),
+                                  laResUnit.retailPrice, laResUnit.netMeteringPrice)
+        to_append = pd.DataFrame([{
+            'PV(kW)': PVSize,
+            'ES(kWh)': ESSize,
+            'Revenue_loss': lostRevenue
+        }])
+
+        revenueLoss = pd.concat([revenueLoss, to_append])
+
+if netMetering == 'y' or 'Y':
+    revenueLoss.to_csv('results\\datafiles\\revenue_loss_nm_la.csv', index=False)
+else:
+    revenueLoss.to_csv('results\\datafiles\\revenue_loss_la.csv', index=False)
+
