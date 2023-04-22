33af5fc01c45c816876bc6aa08ccce33f3a813ae
arun-particlephy <arun.particlephy@tamu.edu>
2023-04-22 10:41:08 -0500

    Normalization option embedded into the class

--- res_unit.py
+++ res_unit.py
@@ -1,6 +1,7 @@
 from pulp import *
 import numpy as np
 from pdrm.bess_model import *
+from pdrm.ceval_indices import *
 from revenue_loss import RevenueLoss
 
 
@@ -10,26 +11,34 @@ class ResUnit:
     """
 
     def __init__(self, load_profile, peak_load, ghi, pv_module_rating, es_module_rating,
-                 retail_price, net_metering_price, soc_min):
-        self.optESSize = None
-        self.optPVSize = None
+                 retail_price, net_metering_price, soc_min, norm: bool):
+
         self.loadProfile = load_profile
         self.peakLoad = peak_load
-        self.loadTimeSeries = load_profile * peak_load
         self.ghi = ghi
-        self.pv_module_rating = pv_module_rating
-        self.es_module_rating = es_module_rating
-        self.retail_price = retail_price
-        self.net_metering_price = net_metering_price
-        self.soc_min = soc_min
-        self.no_pv_modules = None
-        self.no_battery_modules = None
+        self.retailPrice = retail_price
+        self.netMeteringPrice = net_metering_price
+        self.socMin = soc_min
+        if norm:
+            self.PVModuleRating = pv_module_rating / peak_load
+            self.ESModuleRating = es_module_rating / peak_load
+            self.loadTimeSeries = load_profile
+        else:
+            self.loadTimeSeries = load_profile * peak_load
+            self.PVModuleRating = pv_module_rating
+            self.ESModuleRating = es_module_rating
+
+        self.PVSize = None
+        self.ESSize = None
+        self.ESPowerLimit = None
+        self.optESSize = None
+        self.optPVSize = None
 
     def optSizing(self, time_horizon):
 
         # create dictionaries for load and output per module variables
         D = makeDict([range(1, time_horizon + 1)], self.loadTimeSeries)
-        outputPerModule = makeDict([range(1, time_horizon + 1)], self.pv_module_rating * 0.8 * self.ghi)
+        outputPerModule = makeDict([range(1, time_horizon + 1)], self.PVModuleRating * 0.8 * self.ghi)
 
         # create a prob variable to contain the problem
         prob = LpProblem("OptimalSizing", LpMinimize)
@@ -59,7 +68,7 @@ class ResUnit:
             prob += Y[i] <= outputPerModule[i] * X
 
         for j in range(0, time_horizon + 1):
-            prob += self.soc_min * self.es_module_rating * B <= S[j] <= self.es_module_rating * B
+            prob += self.socMin * self.ESModuleRating * B <= S[j] <= self.ESModuleRating * B
 
         # Battery initial condition
         prob += S[0] == S[time_horizon]
@@ -72,31 +81,42 @@ class ResUnit:
 
         print("Status:", LpStatus[prob.status])
 
-        self.no_pv_modules = X.varValue
-        self.no_battery_modules = B.varValue
-
-        self.optPVSize = self.no_pv_modules * self.pv_module_rating
-        self.optESSize = self.no_battery_modules * self.es_module_rating
+        self.optPVSize = X.varValue * self.PVModuleRating
+        self.optESSize = B.varValue * self.ESModuleRating
 
         return self.optPVSize, self.optESSize
 
-    def netLoadNoFailures(self, pv_sizing_factor, es_sizing_factor):
+    def netLoadNoFailures(self):
         soc = 1
-        PVCapacity = self.optPVSize * pv_sizing_factor
-        ESCapacity = self.optESSize * es_sizing_factor
-        ESPowerLimit = 2 * self.peakLoad
         deratingFactor = 0.8
-        PVOutputTimeSeries = PVCapacity * deratingFactor * self.ghi
+        PVOutputTimeSeries = self.PVSize * deratingFactor * self.ghi
 
         netLoad = np.zeros(8760)
         for hour in range(8760):
-            netLoad[hour], soc = bess_operation(ESPowerLimit, soc, self.soc_min, ESCapacity, self.loadTimeSeries[hour],
+            netLoad[hour], soc = bess_operation(self.ESPowerLimit, soc, self.socMin, self.ESSize,
+                                                self.loadTimeSeries[hour],
                                                 PVOutputTimeSeries[hour])
         setattr(self, 'netLoad', netLoad)
+
+        return netLoad
 
     def riskToUtility(self):
 
         defectionCriteria = 1.2  # define something greater than 1
-        risk = RevenueLoss(defectionCriteria, self.loadTimeSeries, self.netLoad, self.retail_price,
-                           self.net_metering_price)/(np.multiply(self.loadTimeSeries, self.retail_price).sum())
+        risk = RevenueLoss(defectionCriteria, self.loadTimeSeries, self.netLoadNoFailures(), self.retailPrice,
+                           self.netMeteringPrice) / (np.multiply(self.loadTimeSeries, self.retailPrice).sum())
         return risk
+
+    def evalReliabilityGridConnected(self):
+        indices = customer_evaluation_grid_connected(cov_convergence=0.075,
+                                                     load_point_repair_time=11.43,
+                                                     load_point_failure_rate=0.305,
+                                                     hourly_load=self.loadTimeSeries,
+                                                     acm_module_rating=self.PVModuleRating,
+                                                     acm_repair_rate=0.0964337280, acm_failure_rate=4.35133e-05,
+                                                     ghi_hourly=self.ghi, bess_state_of_charge_min=self.socMin,
+                                                     bess_repair_rate=0.1, bess_failure_rate=0.0000114155,
+                                                     bess_capacity=self.ESSize, pv_capacity=self.PVSize,
+                                                     bess_power_limit=self.ESPowerLimit)
+        setattr(self, 'indices', indices)
+        return indices
