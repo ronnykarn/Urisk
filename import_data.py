52f386e30e08bcf2fc7844646849335f4b500ccd
arun-particlephy <arun.particlephy@tamu.edu>
2023-04-24 18:00:04 -0500

    read county data in the script

--- import_data.py
+++ import_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 def import_data():
     # data from csv files
+    counties_data = pd.read_csv('datafiles\\counties_data.csv')
     load_profiles = pd.read_csv('datafiles\\load_profiles.csv')
     counties_ghi = pd.read_csv('datafiles\\ghi_counties.csv') / 1000
     parameter_data = pd.read_csv('datafiles\\parameter_data.csv')
@@ -24,7 +25,8 @@ def import_data():
     LPFailureRate = parameter_data.iloc[0]['loadpoint_failure_rate']
     LPRepairTime = parameter_data.iloc[0]['loadpoint_repair_time']
 
-    data = {'countyLoadProfiles': load_profiles,
+    data = {'countiesData': counties_data,
+            'countyLoadProfiles': load_profiles,
             'countyGHI': counties_ghi,
             'PVModuleRating': PVModuleRating,
             'PVFailureRate': PVFailureRate,
