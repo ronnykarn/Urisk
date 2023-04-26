963eee817c850eddae1a58efa7208f82dc125a29
arun-particlephy <arun.particlephy@tamu.edu>
2023-04-26 07:14:09 -0500

    update function

--- risk.py
+++ risk.py
@@ -29,38 +29,37 @@ def RevenueLoss(defection_criteria, demand_load, demand_net, retail_electricity_
     return revenue_loss
 
 
-def riskToUtility(county: str, county_data: dict, samples):
+def riskToUtility(county: str, county_data: dict, samples, netMetering: bool):
 
     risk = np.empty(0)
-    for netMetring in [True, False]:
-        normResUnit = ResUnit(load_profile=county_data['countyLoadProfiles'][county],
-                              ghi=county_data['countyGHI'][county],
-                              pv_module_rating=county_data['PVModuleRating'],
-                              es_module_rating=county_data['ESModuleRating'],
-                              soc_min=county_data['socMin'],
-                              peak_load=1,
-                              retail_price=0.2231,
-                              net_metering_price=0,
-                              norm=True
-                              )
-        normResUnit.ESPowerLimit = 2
-        if netMetring:
-            normResUnit.netMeteringPrice = normResUnit.retailPrice/2
-        else:
-            normResUnit.netMeteringPrice = 0
-        projectedLoss = []
+    normResUnit = ResUnit(load_profile=county_data['countyLoadProfiles'][county],
+                          ghi=county_data['countyGHI'][county],
+                          pv_module_rating=county_data['PVModuleRating'],
+                          es_module_rating=county_data['ESModuleRating'],
+                          soc_min=county_data['socMin'],
+                          peak_load=1,
+                          retail_price=0.2231,
+                          net_metering_price=0,
+                          norm=True
+                          )
+    normResUnit.ESPowerLimit = 2
+    if netMetering:
+        normResUnit.netMeteringPrice = normResUnit.retailPrice / 2
+    else:
+        normResUnit.netMeteringPrice = 0
+    projectedLoss = []
 
-        for sample in samples:
-            normResUnit.PVSize = sample[0]
-            normResUnit.ESSize = sample[1]
-            sampleLoss = RevenueLoss(1.2, demand_load=normResUnit.loadProfile,
-                                     demand_net=normResUnit.netLoadNoFailures(),
-                                     retail_electricity_price=normResUnit.retailPrice,
-                                     net_metering_price=normResUnit.netMeteringPrice)
+    for sample in samples:
+        normResUnit.PVSize = sample[0]
+        normResUnit.ESSize = sample[1]
+        sampleLoss = RevenueLoss(1.2, demand_load=normResUnit.loadProfile,
+                                 demand_net=normResUnit.netLoadNoFailures(),
+                                 retail_electricity_price=normResUnit.retailPrice,
+                                 net_metering_price=normResUnit.netMeteringPrice)
 
-            projectedLoss.append(sampleLoss)
+        projectedLoss.append(sampleLoss)
 
-        avgProjectedLoss = sum(projectedLoss) / samples.shape[0]
-        risk = np.append(risk, avgProjectedLoss / (np.multiply(normResUnit.loadProfile, normResUnit.retailPrice).sum()))
+    avgProjectedLoss = sum(projectedLoss) / samples.shape[0]
+    risk = avgProjectedLoss / (np.multiply(normResUnit.loadProfile, normResUnit.retailPrice).sum())
 
     return risk
