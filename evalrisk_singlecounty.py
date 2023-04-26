f823e692f1a086b1e5c6039a6da1ac92267d2469
arun-particlephy <arun.particlephy@tamu.edu>
2023-04-26 07:13:59 -0500

    update eval

--- evalrisk_singlecounty.py
+++ evalrisk_singlecounty.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from scipy import stats
 
@@ -5,9 +6,6 @@ from helperfuncs import *
 from import_data import import_data
 from risk import *
 from sampling import generateCorrUniSamples
-import random as r
-
-r.seed(12345678)
 
 data = import_data()
 covariance = np.array([[1.579, 1.708], [1.708, 2.25]])
@@ -17,7 +15,7 @@ PVLoc = 0
 ESLoc = 0
 
 riskDf = pd.DataFrame(
-    columns=['scale', 'risk', 'riskNM']
+    columns=['scale', 'risk']
 )
 
 for step in range(1, 5):
@@ -25,21 +23,19 @@ for step in range(1, 5):
     PVLims = getClips(PVLoc, scale, 0, PVClip)
     ESLims = getClips(ESLoc, scale, 0, ESClip)
 
-    covCriteria = 1
+    cov = 1
     riskPerIteration = np.empty(0)
-    riskPerIterationNM = np.empty(0)
     sampleCounter = 0
 
-    while covCriteria > 0.05:
-        samples = 2
+    while cov > 0.05:
+        samples = 1
         sampleCounter = sampleCounter + samples
 
         riskPerSample = np.zeros(samples)
-        riskPerSampleNM = np.zeros(samples)
 
         for i in range(samples):
             # Draw correlated Uniform samples correlated by the covariance matrix
-            corrUniSamples = generateCorrUniSamples(25, covariance)
+            corrUniSamples = generateCorrUniSamples(50, covariance)
 
             # Convert the correlated uniform samples to the PVnorm and ESnorm space using inverse integral transform and
             # with the marginals of PV norm and ES norm
@@ -48,22 +44,18 @@ for step in range(1, 5):
 
             jointSample = np.vstack((PVSample, ESSample)).T
 
-            x = riskToUtility('Los Angeles County', data, jointSample)
-            riskPerSample[i] = x[0]
-            riskPerSampleNM[i] = x[1]
+            riskPerSample[i] = riskToUtility('Los Angeles County', data, jointSample, netMetering=False)
 
         riskPerIteration = np.append(riskPerIteration, riskPerSample)
-        riskPerIterationNM = np.append(riskPerIterationNM, riskPerSampleNM)
         cov = np.sqrt(np.var(riskPerIteration) / sampleCounter) / np.mean(riskPerIteration)
-        covNM = np.sqrt(np.var(riskPerIterationNM) / sampleCounter) / np.mean(riskPerIterationNM)
-        covCriteria = max(cov, covNM)
 
-    avgRisk = riskPerIteration.mean()
-    avgRiskNM = riskPerIterationNM.mean()
-    to_append = pd.DataFrame([dict(scale=scale, risk=avgRisk, riskNM=avgRiskNM)])
+    to_append = pd.DataFrame([{
+        'scale': scale,
+        'risk': riskPerIteration.mean()
+    }])
 
     riskDf = pd.concat([riskDf, to_append])
 
-riskDf.to_csv('results\\datafiles\\risk_scale_la.csv', index=False)
+riskDf.to_csv('results\\datafiles\\risk_scale.csv', index=False)
 
 k = 1
